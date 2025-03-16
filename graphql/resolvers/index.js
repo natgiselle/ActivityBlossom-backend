6632b809be7d801a5332fcf315dc3bfba1b9e118
natalie <natalieortiz292006@outlook.com>
2025-03-16 02:35:02 -0400

    Initial commit for ActivityBlossom backend

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -18,7 +18,7 @@ module.exports = {
     Post: {
         likeCount: (parent) => parent.likes.length,
         commentCount: (parent) => parent.comments.length
-      },
+    },
     Query: {
 
         ...postsResolvers.Query
