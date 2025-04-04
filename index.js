d5a54cfd00640198e2432fd80e92736ffed1d69d
natalie <natalieortiz292006@outlook.com>
2025-04-03 21:56:54 -0400

    adds minor changes

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 //Need to npm install apollo-server
 
 const { ApolloServer } = require('apollo-server');
+const { MONGODB } = require('./config.js')
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/TypeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB, PORT } = require('./config');
 
 // Create Apollo Server instance
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
-  formatError: (err) => {
-    // Don't expose internal server errors to clients
-    if (err.message.startsWith('Database Error: ')) {
-      return new Error('Internal server error');
-    }
-    // Return the original error for all others
-    return err;
-  },
-  plugins: [
-    {
-      requestDidStart: () => ({
-        willSendResponse(requestContext) {
-          // Log any errors that occur
-          const errors = requestContext.response.errors;
-          if (errors) {
-            console.error('GraphQL Errors:', errors);
-          }
-        }
-      })
-    }
-  ]
 });
 
 // Connect to MongoDB and start server
